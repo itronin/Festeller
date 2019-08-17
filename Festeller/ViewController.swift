755add17f99033ad1bb55cd928cdbc558771f734
Igor Tronin <tronin.88@gmail.com>
2019-08-17 21:53:39 +0300

    Initial Commit

---
+++ Festeller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Festeller
+//
+//  Created by Igor Tro on 17/08/2019.
+//  Copyright © 2019 Igor Tro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
